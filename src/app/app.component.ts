9a55b07243d87bc13607428b6c6a4892bcec4666
kahashanmadhuranga <hashanmadhuranga@gmail.com>
2021-01-26 17:01:41 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+// @ts-ignore
+import data from './../assets/data.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'youthtpoia';
+export class AppComponent implements OnInit {
+
+  title = 'Youthtpoia';
+
+  featuredProducts: Array<any> = [];
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    this.featuredProducts = data.featuredProducts;
+  }
 }
